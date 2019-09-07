13fac1dc89dd814eaf4cb5db92046e365681c0d7
Bogdan <bgdnbskkv@gmail.com>
2019-09-07 16:11:44 +0500

    Initial Commit

---
+++ TODOxRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODOxRealm
+//
+//  Created by Bogdan Baskakov on 07/09/2019.
+//  Copyright © 2019 Bogdan Baskakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
